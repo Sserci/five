7743b229e712489c30254bbdc49aaa5d5cf23273
Serci Serge <serge.serci@hotmail.fr>
2022-07-06 10:01:05 +0200

    reservation page ok

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,12 +2,17 @@ class ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new
     @reservation.user = current_user
-    @reservation.event = Event.find(params[:event_id])
+    event = Event.find(params[:event_id])
+    @reservation.event = event
     if @reservation.save
-      redirect_to event_path(@reservation.event)
+      redirect_to event_reservation_path(event, @reservation)
     else
       redirect_to events_path
     end
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
   end
 
   def destroy
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,9 @@ class Event < ApplicationRecord
   def booked?(user)
     Reservation.find_by(user: user, event: self)
   end
+
+  def remaining_places
+    reservations = Reservation.where(event: self)
+    reservations.length
+  end
 end
